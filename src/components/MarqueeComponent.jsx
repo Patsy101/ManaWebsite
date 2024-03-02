1cecbf03404cf54d34cac59f480fdb4634e81df6
John Doe <patricktricenio549@gmail.com>
2024-03-02 18:19:49 +1300

    fix

--- src/components/MarqueeComponent.jsx
+++ src/components/MarqueeComponent.jsx
@@ -12,7 +12,7 @@ const MarqueeComponent = () => {
           Visit us at 240 Ormiston Road Flatbush, Manukau, New Zealand.
           For urgent assistance, call us at 09-558 3817 or use our human live chat support below.
           For non-urgent assistance, email us at kusinaniperla@gmail.com or use our Contact/Reservation Form.
-          We are open from 11:00 AM to 7:00 PM, Tuesday to Sunday.
+          We are open from 11:00 AM to 8:00 PM, Tuesday to Sunday (Monday Closed).
           (Please be advised that Manna Kitchen Ormiston will never ask for your bank card information/physical address via email,phone and human live chat support)
         </p>
       </Marquee>
